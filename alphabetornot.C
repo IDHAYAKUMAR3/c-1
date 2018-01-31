ea1d2be2275cd1fff9431e1d471da7dbe53d7c10
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-01-31 14:52:00 +0530

    Create alphabetornot.C

---
+++ alphabetornot.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+char a;
+scanf("%c",&a);
+if((a>='a'&&a<='z')||(a>='A'&&a<='Z'))
+{
+printf("Alphabet");
+}
+else
+{
+printf("Not an alphabet");
+}
+return 0;
+}
