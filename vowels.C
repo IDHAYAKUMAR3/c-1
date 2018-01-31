3ae53b13b36179b0f970dfc10601872a66c2c8a2
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-01-31 14:30:03 +0530

    Create vowels.C

---
+++ vowels.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a;
+int b,c;
+scanf("%c",&a);
+b=(a=='a'||a=='e'||a=='i'||a=='o'||a=='u');
+c=(a=='A'||a=='E'||a=='I'||a=='O'||a=='U');
+if(b||c)
+{
+printf("VOWEL");
+}
+else
+{
+printf("CONSONANT");
+}
+return 0;
+}
