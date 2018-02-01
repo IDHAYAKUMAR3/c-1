426e178e870b32b7bc77a61f7f007d4bae0f796c
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-02-01 19:58:23 +0530

    Create sumofarray.c

---
+++ sumofarray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[],b,c,i,s=0;
+scanf("%d%d",&b,&c);
+printf("\n");
+for(i=0;i<5;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<c:i++)
+{
+s=s+a[i];
+}
+printf("\n%d",s);
+return 0;
+}
