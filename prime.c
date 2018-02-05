badf872bc97d837c70fbbb14cb50695a3bdc6e8c
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-02-05 14:12:30 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,i,d=0;
+scanf("%d",&a);
+b=a/2;
+for(i=2;i<=b;i++)
+{
+if(a%i==0)
+{
+d=1;
+break;
+}
+}
+if(d==0)
+printf("YES");
+else
+printf("NO");
+return 0;
+}
