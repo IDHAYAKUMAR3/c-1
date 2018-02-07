08162f6033318ca407ea15383008adf3db19b9ca
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-02-07 14:31:11 +0530

    Update max_array.c

--- max_array.c
+++ max_array.c
@@ -1,9 +1,9 @@
 #include<stdio.h.
 int main()
 {
-int j=1,n,a[20],i;
+int j=1,n,a[20],i,c;
 scanf("%d",&n);
-for(i=0;i<ni++)
+for(i=0;i<n;i++)
 {
 scanf("%d",a[i]);
 }
@@ -12,4 +12,13 @@ for(i=0;i<n;i++)
 if(a[i]>a[j])
 {
 c=a[i];
+}
+else
+{
+c=a[j];
+}
+j++;
+}
+printf("%d",c);
+return 0;
 }
